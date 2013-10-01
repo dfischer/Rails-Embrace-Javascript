87947a875f2756e2e89ac6b39814eac73ba5cb74
Rafael George <george.rafael@gmail.com>
2013-10-01 17:05:41 -0400

    Step definitions supporting there's no projects

--- timesheet/features/step_definitions/view_projects_steps.rb
+++ timesheet/features/step_definitions/view_projects_steps.rb
@@ -1,13 +1,12 @@
 Given(/^there are no projects$/) do
-    pending # express the regexp above with the code you wish you had
 end
 
 When(/^I go to the all projects end point$/) do
-    pending # express the regexp above with the code you wish you had
+  visit all_projects_url
 end
 
 Then(/^I should retrieve a there are no projects$/) do
-    pending # express the regexp above with the code you wish you had
+  page.content.should == {message:'There are no projects'} 
 end
 
 Given(/^there are some projects$/) do
