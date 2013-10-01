9ad42e083af14dc26ee259b4ff013c3470402fd0
Rafael George <george.rafael@gmail.com>
2013-10-01 15:15:09 -0400

    Add Rails application API

---
+++ timesheet/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timesheet::Application.config.secret_token = '0fa4a935739c390ff11b915a3dd2f23598db295ae2265d7afa8428999af61347269d9c593402f638c1501b4c8a3ec7a47b0318fe772792e6922225ce5e9f123c'
