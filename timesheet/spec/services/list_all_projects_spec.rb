924fdd5bba9b321814040d5ebb118f2f44866124
Rafael George <george.rafael@gmail.com>
2013-10-07 17:19:56 -0400

    Promote ListAllProjects to be a full fledge object

--- timesheet/spec/services/list_all_projects_spec.rb
+++ timesheet/spec/services/list_all_projects_spec.rb
@@ -4,10 +4,10 @@ describe ListAllProjects, "#call" do
   let(:service) { ListAllProjects.new }
 
   context 'when there are no projects' do 
-
     it 'returns nothing' do 
       expect(service.call).to eql []
     end
   end
+
 
 end
